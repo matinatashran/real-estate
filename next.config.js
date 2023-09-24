d3ce7d8039ebaaf00aef382ad0d026b7a481802e
matin atashran <matinatashran.5160@gmail.com>
2023-09-24 12:10:28 +0330

    Fixed some bugs

--- next.config.js
+++ next.config.js
@@ -6,10 +6,17 @@ const nextConfig = {
   async headers() {
     return [
       {
+        // matching all API routes
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          // omitted for brevity...
+        ],
+      },
+      {
+        source: "/api/special-data",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "false" },
+          { key: "Access-Control-Allow-Origin", value: "https://example.com" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,DELETE,PATCH,POST,PUT",
@@ -17,7 +24,7 @@ const nextConfig = {
           {
             key: "Access-Control-Allow-Headers",
             value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+              "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
           },
         ],
       },
---
+++ src/app/api/special-data/middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+    // retrieve the current response
+    const res = NextResponse.next()
+
+    // if the incoming is for the desired API endpoint
+    if (req.nextUrl.pathname === '/special-data') {
+        res.headers.append('Access-Control-Allow-Credentials', "false")
+        res.headers.append('Access-Control-Allow-Origin', 'https://example.com') 
+        res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+        res.headers.append('Access-Control-Allow-Headers', 'Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date')
+    } else {
+        // generic CORS policy omitted for brevity....
+    }
+
+    return res
+}
+
+// specify the path regex to apply the middleware to
+export const config = {
+    matcher: '/api/:path*',
+}
