1fa4f2795072c92e35dc1692beb2ab61381fba9e
matin atashran <matinatashran.5160@gmail.com>
2023-09-24 15:02:26 +0330

    Apply some changes

--- next.config.js
+++ next.config.js
@@ -3,26 +3,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  async headers() {
-    return [
-      {
-        source: "/api/special-data",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "false" },
-          { key: "Access-Control-Allow-Origin", value: "https://example.com" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
